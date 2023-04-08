830e258a7e6f3c4fdb967785db83f921cfa7f4c3
alessio <alessiomungelli@gmail.com>
2023-04-08 16:58:36 +0200

    follow me on instagram style added with animations

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+    const beforeMex = "Seguimi su instagram!";
+    const afterMex = "Divertiti con le mie creazioni!";
     $(".container").hide();
     $("#instagram").hide();
     $("#ig").hide();
@@ -8,5 +10,35 @@ $(document).ready(function(){
             $("#instagram").fadeIn(1000);
             $("#ig").show();
         });
-    })
+    });
+    $("#instagram").click(function(){
+        if ($("#instagram").text() == beforeMex){
+          // *** State Change: To Following ***      
+          // We want the button to squish (or shrink) by 10px as a reaction to the click and for it to last 100ms    
+          $("#instagram").animate({ width: '-=10px' }, 100, 'easeInCubic', function () {});
+          
+          // then now we want the button to expand out to it's full state
+          // The left translation is to keep the button centred with it's longer width
+          $("#instagram").animate({ width: '+=45px', left: '-=15px' }, 600, 'easeInOutBack', function () { 
+            $("#instagram").css("color", "#fff");
+            $("#instagram").text(afterMex);
+    
+            // Animate the background transition from white to green. Using JQuery Color
+            $("#instagram").animate({
+              backgroundColor: "#2EB82E",
+              borderColor: "#2EB82E"
+            }, 1000 );
+          });
+        }else{
+          
+          // *** State Change: Unfollow ***     
+          // Change the button back to it's original state
+          $("#instagram").animate({ width: '-=25px', left: '+=15px' }, 600, 'easeInOutBack', function () { 
+            $("#instagram").text(beforeMex);
+            $("#instagram").css("color", "#3399FF");
+            $("#instagram").css("background-color", "#ffffff");
+            $("#instagram").css("border-color", "#3399FF");
+          });
+        }
+      }); 
 });
